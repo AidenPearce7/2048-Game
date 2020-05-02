01e82a950bd580ab4b0f5778657520fe7f9ea41b
Aiden Pearce <30537758+AidenPearce7@users.noreply.github.com>
2020-05-02 20:53:50 +0200

    Changed file layout

--- 2048Game/Number.cpp
+++ 2048Game/Number.cpp
@@ -1,18 +1,4 @@
-
-class Number
-{
-private:
-     int number;
-     bool hasJoinedInThisTurn;
-public:
-    int GetNumber();
-    void SetNumber(int value);
-    void Join(Number number);
-    bool Compare(Number number);
-    void ResetTurn();
-    Number();
-    ~Number();
-};
+#include "Number.h"
 
 Number::Number()
 {
